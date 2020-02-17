7b24e9593dfa6cac8eb160b82ce1282e8fe5bcb7
Prem-K-Tamang <ktamangprem@gmail.com>
2020-02-17 12:27:45 +0545

    API request mechanism added

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    context = {}
+    #http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=10&API_KEY=888326FB-33F0-4CE5-9A35-E7F73C64A1A4
+    import json
+    import requests
+    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=10&API_KEY=888326FB-33F0-4CE5-9A35-E7F73C64A1A4")
+
+    try:
+        global api
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    context = {'api':api}
     return render(request, 'home.html', context)
 
 
